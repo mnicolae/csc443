8ece31c261db4dc64a4b4562d54ad874d07319cc
Mihai Nicolae <m.nicolae91@gmail.com>
2014-10-20 14:23:48 -0400

    some more starting code.

--- a2/msort.cc
+++ a2/msort.cc
@@ -6,15 +6,27 @@
 #include "library.h"
 #include "json/json.h"
 
+#include <string.h>
+#include <time.h>
+#include <sys/timeb.h>
+
 using namespace std;
 
 int main(int argc, const char* argv[]) {
+  struct timeb start, end;
+  long start_time, end_time;
+
   if (argc < 7) {
     cout << "ERROR: invalid input parameters!" << endl;
     cout << "Please enter <schema_file> <input_file> <output_file> <mem_capacity> <k> <sorting_attributes>" << endl;
     exit(1);
   }
   string schema_file(argv[1]);
+  //string input_file(argv[2]);
+  //string output_file(argv[3]);
+  //int mem_capacity(argv[4]);
+  //int k(argv[5]);
+  //string sorting_attributes[];
 
   // Parse the schema JSON file
   Json::Value schema;
@@ -28,6 +40,13 @@ int main(int argc, const char* argv[]) {
     exit(1);
   }
 
+  // Initialize the Schema structure
+  Attribute my_attribute;
+  Schema my_schema;
+
+  my_schema.nattrs = schema.size();
+  my_schema.n_sort_attrs = (argc - 7) + 1;
+
   // Print out the schema
   string attr_name;
   int attr_len;
@@ -35,10 +54,23 @@ int main(int argc, const char* argv[]) {
     attr_name = schema[i].get("name", "UTF-8" ).asString();
     attr_len = schema[i].get("length", "UTF-8").asInt();
     cout << "{name : " << attr_name << ", length : " << attr_len << "}" << endl;
+
+    //memcpy(my_attribute.name, attr_name.c_str(), attr_name.length() + 1); 
   }
+  
+  // TODO: Parse the input CSV file
+
+  ftime(&start);
+  start_time = start.time * 1000 + start.millitm; 
 
   // Do the sort
   // Your implementation
-  
+
+  ftime(&end);
+  end_time = end.time * 1000 + end.millitm;  
+
+  printf("\nTIME: %ld miliseconds\n", end_time - start_time);
+
   return 0;
 }
+
