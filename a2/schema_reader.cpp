a5b6ed81c31d5dc07fe8b0361d24827a304ef9dd
Huy Bui <mailtohuy@gmail.com>
2014-10-24 17:10:05 -0400

    added SchemaReader::addSortingAttributes; *change declarations*of Attribute and Schema

--- a2/schema_reader.cpp
+++ a2/schema_reader.cpp
@@ -1,30 +1,30 @@
 #include "schema_reader.h" 
 
-Attribute* SchemaReader::createAttribute(const char* name, int len, int type) {
+Attribute* SchemaReader::createAttribute(std::string name, int len, int type) {
     Attribute* attr = new Attribute;
-    attr->name = (char*) malloc(strlen(name));
-    strcpy(attr->name, name);
+    attr->name.append(name);
     attr->length = len;
     attr->type = type;
     return attr;
 }
 
-SchemaReader::SchemaReader(char* filename) {
-	schema_file = new std::ifstream(filename, std::ifstream::binary);
+SchemaReader::SchemaReader(std::string filename) {
+	schema_file = new std::ifstream(filename.c_str(), std::ifstream::binary);
     schema = read();
     schema_file->close();
     delete schema_file;
+    
+    // initialize the sorting attributes
+    schema->n_sort_attrs = 0;
+    schema->sort_attrs = NULL;
 }
 
 SchemaReader::~SchemaReader() { /* do nothing */}
 
 void SchemaReader::deallocate() { 
     // de-allocate Attributes and Schema
-	Attribute* attr;
     for(int i=0; i < schema->nattrs; i++) {
-    	attr = schema->attrs[i];
-    	free(attr->name);
-    	delete attr;
+    	delete schema->attrs[i];
     }
     delete schema;
 }
@@ -35,7 +35,8 @@ Schema* SchemaReader::read() {
   bool parsingSuccessful = reader.parse(*schema_file, root, false);
 
   if (!parsingSuccessful) {
-    std::cout << "reading json file failed\n" ;
+    std::cout << "Failed to read schema.\nExitting...\n";
+    exit(1);
   }
 
   Schema* retval = new Schema;
@@ -52,13 +53,15 @@ Schema* SchemaReader::read() {
 	length = root[i].get("length","").asInt();
 	
 	if (type == "string") {
-		retval->attrs[i] = createAttribute(name.c_str(), length, STRING);
+		retval->attrs[i] = createAttribute(name, length, STRING);
 	}
+	
 	if (type == "integer") {
-		retval->attrs[i] = createAttribute(name.c_str(), length, INTEGER);
+		retval->attrs[i] = createAttribute(name, length, INTEGER);
 	}
+	
 	if (type == "float") {
-		retval->attrs[i] = createAttribute(name.c_str(), length, FLOAT);
+		retval->attrs[i] = createAttribute(name, length, FLOAT);
 	}
   }
   
@@ -70,12 +73,42 @@ Schema* SchemaReader::getSchema() {
 	return schema;
 }
 
-//
+void SchemaReader::addSortingAttributes(std::string csvAttrList) {
+	std::stringstream list(csvAttrList);
+	std::string attr;
+	std::vector<int> sort_indices;
+	int i=0;
+	int count=0;
+	
+	while (list.good()) {
+		getline(list, attr, ',');
+		if (attr.length() == 0) {
+			continue;
+		}
+		count ++; 
+		for(i=0; i < schema->nattrs; i++) {
+			if (schema->attrs[i]->name == attr) {
+				sort_indices.push_back(i);
+			}
+		}		
+	}
+	
+	if (count > 0) {
+		schema->n_sort_attrs = count;
+		schema->sort_attrs = new int[count];
+		for(i=0; i < count; i++) {
+			schema->sort_attrs[i] = sort_indices[i];
+		}		
+	}
+}
+
+
 // /* g++ -Wall -I. -g3 -o reader schema_reader.cpp jsoncpp.o */
 //
 //int main() {
 //	SchemaReader reader("schema_example.json");
-//  Schema* s = reader.getSchema(); // get a pointer to the schema
+//	Schema* s = reader.getSchema(); // get a pointer to the schema
+//	reader.addSortingAttributes("student_number,start_year");
 //	reader.deallocate(); // free memory that "s" occupies
 //	return 0;
 //}
--- a2/schema_reader.h
+++ a2/schema_reader.h
@@ -2,19 +2,20 @@
 #include <fstream> 
 #include <json/json.h>
 #include <cstring>
+#include <sstream>
 
 enum Types {INTEGER = 0, STRING = 1, FLOAT = 2};
 
 typedef struct {
-	char* name;
+	std::string name;
 	int length;
 	int type;
 } Attribute;
 
 typedef struct {
-	Attribute** attrs;
+	Attribute **attrs;
     int nattrs;
-	int sort_attrs[];
+	int *sort_attrs;
 	int n_sort_attrs;
 } Schema;
 
@@ -22,11 +23,12 @@ class SchemaReader{
 private:
 	std::ifstream* schema_file;
     Schema* schema;
-    Attribute* createAttribute(const char* name, int len, int type);
+    Attribute* createAttribute(std::string name, int len, int type);
 	Schema* read();
 public:
-	SchemaReader(char* filename);
+	SchemaReader(std::string);
 	~SchemaReader();
     Schema* getSchema();
+    void addSortingAttributes(std::string csvAttrList);
     void deallocate();
 };
