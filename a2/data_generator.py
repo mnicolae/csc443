022da9ccf3ef8e4aebde49b202d89b9c2feb6fcf
Huy Bui <huybui@Huys-MacBook-Air.local>
2014-10-29 13:09:29 -0400

    data_generator now forks a process for each attribute

--- a2/data_generator.py
+++ a2/data_generator.py
@@ -1,11 +1,13 @@
 from scipy import stats
 from scipy.stats import norm
+from multiprocessing import Pool
 
 import math
 import random
 import numpy
 import csv
 import os
+import sys, json
 
 def write2file(filename, records):
   f = open(filename, "w")
@@ -13,6 +15,53 @@ def write2file(filename, records):
     f.write(str(r) + '\n')
   f.close()
 
+'''
+Generate string with length len
+'''
+letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numbers = "0123456789"
+def generate_strings(nrecords, len):
+    tmp = []
+    for k in range(nrecords):
+        tmp.append(''.join([random.choice("ABCDEFGHIJKLMNOPQRSTUVWXYZ") for j in range(len)]))
+    return tmp
+
+'''
+Generate uniformly distributed integers
+'''
+def generate_ints(nrecords, min, max):
+    tmp = []
+    tmp.extend(numpy.random.randint(min, max + 1, nrecords))
+    return tmp
+
+'''
+Generate float using normal distribution
+'''
+def generate_floats(nrecords, mu, sigma):
+    rv = norm(mu, sigma)
+    a = rv.rvs(nrecords)
+    tmp = [math.ceil(x*100)/100 for x in a]
+    return tmp
+
+'''
+generate data and create a file
+'''
+def create_attr_file(data):
+    attribute = data[0]
+    nrecords = data[1]
+    if attribute["type"] == "string":
+        tmp = generate_strings(nrecords, attribute["length"])
+        write2file(attribute["name"] + ".tmp", tmp)
+    elif attribute["type"] == "integer":
+        if "distribution" in attribute:
+            if attribute["distribution"]["name"] == "uniform":
+                tmp = generate_ints(nrecords, attribute["distribution"]["min"], attribute["distribution"]["max"])
+                write2file(attribute["name"] + ".tmp", tmp)
+    elif attribute["type"] == "float":
+        if "distribution" in attribute:
+            if attribute["distribution"]["name"] == "normal":
+                tmp = generate_floats(nrecords, attribute["distribution"]["mu"], attribute["distribution"]["sigma"])
+                write2file(attribute["name"] + ".tmp", tmp)
 
 '''
 You should implement this script to generate test data for your
@@ -39,29 +88,15 @@ def generate_data(schema, out_file, nrecords):
   The output file must be in csv format, with a new line
   character at the end of every record.
   '''
-  letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  numbers = "0123456789"
   
-  print "Generating %d records" % nrecords
+  print "Generating %d records ... " % nrecords
   
-  for attribute in schema:
-    if attribute["type"] == "string":
-      tmp = []
-      for k in range(nrecords):
-        tmp.append(''.join([random.choice(letters) for j in range(attribute["length"])]))
-      write2file(attribute["name"] + ".tmp", tmp)
-    elif attribute["type"] == "integer":
-      if "distribution" in attribute:
-        if attribute["distribution"]["name"] == "uniform":
-          tmp = []
-          tmp.extend(numpy.random.randint(attribute["distribution"]["min"], attribute["distribution"]["max"] + 1, nrecords))
-          write2file(attribute["name"] + ".tmp", tmp)
-    elif attribute["type"] == "float":
-      if "distribution" in attribute:
-        if attribute["distribution"]["name"] == "normal":
-          rv = norm(attribute["distribution"]["mu"], attribute["distribution"]["sigma"])
-          a = rv.rvs(nrecords)
-          write2file(attribute["name"] + ".tmp", [math.ceil(x*100)/100 for x in a])
+  # create a worker process for each attribute
+  pool = Pool()
+  args = zip(schema, [nrecords for i in range(len(schema))])
+  pool.map(create_attr_file, args)
+  pool.close()
+  pool.join()
 
   # open the temporary column files
   files = []
@@ -85,7 +120,6 @@ def generate_data(schema, out_file, nrecords):
 
 
 if __name__ == '__main__':
-  import sys, json
   if not len(sys.argv) == 4:
     print "data_generator.py <schema json file> <output csv file> <# of records>"
     sys.exit(2)
